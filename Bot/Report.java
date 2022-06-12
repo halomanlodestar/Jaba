086c812d51032955ecaec0ed8af5b08248dd925b
halomanlodestar <71826421+halomanlodestar@users.noreply.github.com>
2022-06-12 18:31:26 +0530

    Update

--- Bot/Report.java
+++ Bot/Report.java
@@ -1,57 +1,28 @@
 package Bot;
 import java.util.*;
-import java.io.*;
+import java.util.stream.Collectors;
 
 public class Report {
-    // Instances
-    public String Username;
-    public String Broken;
-    public String Region;
-    static int count = 1;
-
-    // Dictionary
-    static Dictionary<Integer, String> ListUserNames = new Hashtable<>();
-    static Dictionary<Integer, String> ListRegion = new Hashtable<>();
-
+    String Username;
+    Region Region;
     // Constructor
-    public Report(String username, String broken, String region) {
+    public Report(String username, Report.Region region) {
         this.Username = username;
-        this.Broken = broken;
         this.Region = region;
-        ListUserNames.put(count, Username);
-        ListRegion.put(count, region);
-        count++;
-        writeList();
     }
 
-    public int AllReports() {
-        return count;
+    public void Execution(List<Report> Players) {
+        Players.stream()
+            .filter(player -> Region.equals(player.Region))
+            .collect(Collectors.toList())
+            .forEach(System.out::println);
     }
-
-    public String getRegion(String s) {
-        return Region;
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return (Username + " : " + Region);
     }
-    
-    public static String getList() {
-        StringBuffer s = new StringBuffer();
-        for (int i = 1; i < count; i++) {
-            String r = ("[" + i + "] : " + ListUserNames.get(i) + " : " + ListRegion.get(i));
-            System.out.println(r);
-            s.append(r +'\n');
-        }
-        //System.out.println("\n \n\n\n" + s);
-        return (s.toString());
-    }
-    private void writeList() {
-        try {
-            File Lists = new File("D:\\context\\GitHub\\jaba\\Bot\\List.txt");
-            FileWriter ListWrite = new FileWriter(Lists);
-            ListWrite.write(getList());
-            ListWrite.close();
-            System.out.println("Successfully Updated List");
-        }
-        catch (IOException e){
-            System.out.println("Error : " + e);
-        }
+    public static enum Region {
+        AS, EU, NA
     }
 }
--- Test/Main.java
+++ Test/Main.java
@@ -1,23 +1,14 @@
 package Test;
-import Bot.*;
 import java.util.*;
+import Bot.*;
 // Elements
+import Bot.Report.Region;
 
 public class Main {
 
     public static void main(String agrs[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Report in Following Format ::");
-        System.out.println("Username : ");
-        System.out.println("Rule Broken :");
-        System.out.println("Region : ");
-        String username = sc.nextLine();
-        String broken = sc.nextLine();
-        String region = sc.nextLine();
-        new Report(username, broken, region);
-        System.out.println("--------------------\nThanks for Reporting. \n--------------------");
-        Report.getList();
-        //
-        sc.close();
+        List<Report> L = List.of(
+            Report("LoadStar2476", Region.AS)
+        ); 
     }
 }
