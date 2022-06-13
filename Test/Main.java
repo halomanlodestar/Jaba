536087d36d1f205b150c5f0333789eba08872222
halomanlodestar <71826421+halomanlodestar@users.noreply.github.com>
2022-06-13 10:39:36 +0530

    Working Reports and Filter

--- Test/Main.java
+++ Test/Main.java
@@ -1,15 +1,14 @@
 package Test;
 import java.util.*;
-import Bot.Report;
-import Bot.Report.Regions;
-
+import Bot.*;
+import Bot.Report.*;
 class Main {
     public static void main(String[] args) {
         List<Report> Players = List.of(
             new Report("LoadStar2476", Regions.AS),
             new Report("ELite0412", Regions.AS),
-            new Report("Ambiennt", Regions.AS)
+            new Report("Ambiennt", Regions.NA)
         );
-        String n = Filter(Players, Regions.AS);
+        Report.Filter(Players, Regions.NA);
     }
 }
